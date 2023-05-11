53f291bb32977f248f281a1021bcdce817320b0b
OksanaFedorii <133260204+OksanaFedorii@users.noreply.github.com>
2023-05-11 21:53:55 +0300

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -2,11 +2,9 @@
 required_distance = float(input("Введіть дистанцію, яку необхідно здолати: "))
 fuel_consumption = float(input("Введіть витрату палива на 100 км: "))
 fuel_price = float(input("Введіть ціну палива за літр: "))
-
 # Визначаємо скільки літрів топлива піде на дану дистанцію
 amount_of_fuel = fuel_consumption / 100 * required_distance
 print(round(amount_of_fuel, 2))
-
 # Визначаємо загальні витрати на топливо в даній поїздці
 costs = amount_of_fuel * fuel_price
 print(round(costs, 2))
